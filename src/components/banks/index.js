2124b2d408e2f4eb5d9a9aa37a888549d0337542
Kunal Nagdev <nagdevkunal2@gmail.com>
2022-11-16 17:09:02 -0600

    updated layout with loading

--- src/components/banks/index.js
+++ src/components/banks/index.js
@@ -2,7 +2,6 @@ import { React, useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { map } from 'lodash';
 
-import Loading from "../loading";
 import { getAllBanks } from "../../graphql/queries/bankQueries";
 import Layout from "../layout";
 import './index.css'
@@ -16,9 +15,8 @@ const Home = () => {
   }, [data]);
 
   return (
-    <Layout>
+    <Layout isLoading={loading}>
       <h1>All Banks</h1>
-      <Loading isLoading={loading} />
       <div>
         {map(banks, bank => (
           <h4 key={bank.BANK_ID}>{bank.BANK_NAME}</h4>
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,10 +2,12 @@ import { React } from 'react';
 
 import Header from './header';
 import Sidebar from './sidebar';
+import Loading from './loading';
 import './index.css'
 
-const Layout = ({ children }) => (
+const Layout = ({ children, isLoading }) => (
   <>
+    <Loading isLoading={isLoading} />
     <Header />
     <div className='body'>
       <Sidebar />
